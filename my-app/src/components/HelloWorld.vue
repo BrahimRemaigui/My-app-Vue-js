ec8afc41ec0fc6e3299103c961a02044fb6e14df
PC-IT <ibrahimremaigui@gmail.com>
2023-05-28 22:50:01 +0100

    add folder

---
+++ my-app/src/components/HelloWorld.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="back">
+    <div class="model" :class="{sale : theme ==='dark'}">
+      <h1>Helo to page of brahim</h1>
+      <p>i'm learning the vue js with css there is tools of frentend devloper</p>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props:['msg','theme']
+}
+</script>
+<style  scoped>
+.model.sale{
+background: rgb(36, 35, 35);
+color: white;
+
+}
+
+.back{
+  background: rgb(0, 0, 0,0.5);
+  width: 100%;
+  height: 100%;
+  position: fixed;
+}
+.model{
+  top: 0;
+  width: 450px;
+  height: 250px;
+  margin: 100px auto;
+  background: white;
+  border-radius:10px ;
+}
+
+
+</style>
