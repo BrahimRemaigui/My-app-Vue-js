e35d8e3589202bfe139bfac5afc7ff39b4a731ae
unknown <ibrahimremaigui@gmail.com>
2023-06-28 19:00:25 +0200

    last change

--- my-app/src/App.vue
+++ my-app/src/App.vue
@@ -13,20 +13,26 @@
     
     </HelloWorld>
     </div>
+
+    <button @click="ShowModel2" class="Butt">Sig In </button>
+    <div v-show="model2">
+     <Sigin></Sigin>
+    </div>
 </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
-
+import Sigin from './components/SigIn.vue'
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,Sigin
   },
   data(){
     return{
       showModel : false,
+      model2: false,
       showWord:'Click to show Model',
       msg:"Welcome to Your Vue.js App"
     }
@@ -37,6 +43,9 @@ export default {
         this.showWord='Click to hide Model'
       else
       this.showWord='Click to show Model'
+    },
+    ShowModel2(){
+      this.model2=!this.model2;
     }
   }
 }
@@ -60,6 +69,7 @@ export default {
   font-weight: 600;margin-bottom: 20px;
   font-size:16px ;
   transition: 0.5s;
+  margin-left: 10px;
 }
 .Butt:hover{
   transform: scale(1.1) rotate(4deg);
---
+++ my-app/src/components/SigIn.vue
@@ -0,0 +1,70 @@
+<template>
+  <h1>Sig In </h1>
+  <form>
+  <label for="">Email</label>
+  <input type="text" v-model="email" >
+
+  <label for="">Password</label>
+  <input type="Password" v-model="password" >
+  <label >Role</label>
+  <select v-model="role">
+    <option value="devloper"> Web Devloper</option>
+    <option value="designer"> Web Designer</option>
+  </select>
+  <p>Your email is {{ email }}</p>
+  <p>Your role is {{ role }}</p>
+
+
+</form>
+</template>
+
+<script>
+export default {
+data(){
+    return{
+        email:'',
+        password:'',
+        role:''
+    }
+}
+}
+</script>
+
+<style>
+form{
+    max-width:420px;
+    margin:30px auto;
+    background: white;
+    text-align: left;
+    padding:40px;
+    border-radius:10px;
+
+}
+label{
+    color:#171515;
+    display: inline-block;
+    margin:25px 0 15px;
+    font-size:0.6em;
+    text-transform: uppercase;
+    letter-spacing: 1px;
+    font-weight: bold;
+
+}
+input{
+    display: block;
+    padding:10px 6px;
+    width:100%;
+    box-sizing: border-box;
+    border:none ;
+    border-bottom: 1px solid #ddd ;
+    color:#555;
+}
+input[type='checkbox']{
+    display: inline-block;
+    width:16px;
+    margin: 0 10px 0 0;
+    position: relative;
+    top: 2px;
+}
+
+</style>
